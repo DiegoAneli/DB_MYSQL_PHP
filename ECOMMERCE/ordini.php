b3531f0a277a2b7230991105c8cdd78781c67bff
DiegoAneli <diegoaneli@gmail.com>
2025-11-13 16:59:07 +0100

    delete done

--- ECOMMERCE/ordini.php
+++ ECOMMERCE/ordini.php
@@ -2,11 +2,9 @@
     //importo il file db
     require 'db.php';
 
-
     $contatto_id = $_GET['id']; // recupero l id del contatto
 
     $contatto = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM contatti WHERE id=$contatto_id"));
-
 
     //salvo in una variabile $ordini i risultati della query
     $ordini = mysqli_query($conn, "SELECT * FROM ordini WHERE contatto_id=$contatto_id"); // query per prendermi tutta la tabella ordini
@@ -38,6 +36,7 @@
                 <th>Prodotto</th>
                 <th>Quantità</th>
                 <th>Data</th>
+                <th>Actions</th>
             </tr>
 
             <?php  while ($row = mysqli_fetch_assoc($ordini)) :  ?>
@@ -48,7 +47,22 @@
                 <td><?= $row['quantita'] ?></td>
                 <td><?= $row['data_di_ordine'] ?></td>
 
+                <td class="actions">
+
+                    <!--Modifica dell ordine-->
+                    <a href="modifica_ordine.php?id=<?= $row['id']  ?>">🖊️</a>
+                    <!--Elimina ordine-->
+                    <a href="elimina_ordine.php?id=<?= $row['id']  ?>"
+                    onclick="return confirm('Sei sicuro di voler eliminare questo ordine?')">🗑️</a>
+
+                </td>
+
+
+
+
             </tr>
+
+            
 
 
 
